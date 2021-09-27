aa53c73a6eef6dd2272c7413f9a35907235949f4
tnguyen10 <tnguyen1@ualberta.ca>
2021-09-27 17:31:46 -0600

    feat: redo landing page layout, move navigation buttons to top

--- classhub/src/App.js
+++ classhub/src/App.js
@@ -12,51 +12,22 @@ const appStyle = {
   borderColor: "grey",
 };
 
-const columnStyle = {
+const bodyStyle = {
   border: "solid",
   borderColor: "#4dabf5",
   minHeight:"65vh",
   marginLeft: "1%",
   borderWidth: "1px",
   borderRadius: "5px",
-};
-
-const innerStyle = {
-  marginLeft: "3%",
-  borderColor: "grey",
-  padding: "10px",
-  margin: "10px",
-};
-
-const buttonStyle = {
-  width: "100%",
-  padding: "10%",
-  marginBottom: "10%",
+  marginTop: "10px"
 };
 
 export default function App() {
   return (
     <div style={appStyle}>
-      <div style={innerStyle}>
         <Overview />
-      </div>
-      <div style={innerStyle}>
-        <Grid container>
-          <Grid item xs={3}>
-            <Button variant="outlined" startIcon={<PersonIcon/>} style={buttonStyle}>
-              Profile
-            </Button>
-            <Button variant="outlined" startIcon={<CalendarTodayIcon/>} style={buttonStyle}>
-              Class Schedule
-            </Button>
-            <Button variant="outlined" startIcon={<SearchIcon/>} style={buttonStyle}>
-              Class Searches
-            </Button>{" "}
-          </Grid>
-          <Grid item xs={9}>
-            <div style={columnStyle}>good morning</div>
-          </Grid>
-        </Grid>
+      <div style={bodyStyle}>
+        Good morning
       </div>
     </div>
   );
--- classhub/src/components/Overview.js
+++ classhub/src/components/Overview.js
@@ -2,6 +2,10 @@ import Avatar from "@mui/material/Avatar";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import LogoutIcon from "@mui/icons-material/Logout";
+import PersonIcon from "@mui/icons-material/Person";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import SearchIcon from "@mui/icons-material/Search";
+import ButtonGroup from "@mui/material/ButtonGroup";
 
 const fontStyle = {
   fontFamily: "Verdana",
@@ -9,9 +13,9 @@ const fontStyle = {
   marginBottom: "-10px",
 };
 
-const logoutButtonStyle = {
-  marginTop:"10px"
-}
+const buttonsStyle = {
+  marginTop: "80px",
+};
 
 export default function Overview() {
   return (
@@ -20,15 +24,18 @@ export default function Overview() {
         <Grid item xs={2}>
           <Avatar sx={{ width: 150, height: 150 }}></Avatar>
         </Grid>
-        <Grid item xs={8}>
+        <Grid item xs={3}>
           <h1 style={fontStyle}>First Last</h1>
           <h3 style={fontStyle}>ID: 1234567890</h3>
           <p style={fontStyle}>Program Name | Year 4</p>
         </Grid>
-        <Grid item xs={2}>
-          <Button variant="outlined" endIcon={<LogoutIcon/>} style={logoutButtonStyle}>
-            Logout
-          </Button>
+        <Grid item xs={7} style={buttonsStyle}>
+          <ButtonGroup variant="outlined" aria-label="outlined button group">
+            <Button startIcon={<PersonIcon />}>Profile</Button>
+            <Button startIcon={<CalendarTodayIcon />}>Class Schedule</Button>
+            <Button startIcon={<SearchIcon />}>Class Searches</Button>
+            <Button endIcon={<LogoutIcon />}>Logout</Button>
+          </ButtonGroup>
         </Grid>
       </Grid>
     </div>
